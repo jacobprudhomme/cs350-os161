dde7b6fe47864d96127e3824bab479fb82d03cb1
Jacob Prud'homme <jacob.pkmn@gmail.com>
2019-02-25 18:30:34 -0500

    [A2a.2] included `trapframe.h`, since referencing `struct trapframe`

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -9,6 +9,7 @@
 #include <thread.h>
 #include <addrspace.h>
 #include <copyinout.h>
+#include <mips/trapframe.h>
 #include "opt-A2.h"
 
   /* this implementation of sys__exit does not do anything with the exit code */
